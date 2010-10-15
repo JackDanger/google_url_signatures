4ce89cef7887adb655a9726dbd7c78e87543643d
Jack Danger Canty <gitcommit@6brand.com>
2010-10-15 13:03:00 -0700

    creating GoogleUrlSignatures.sign(url, key) method
    Tests passing.

--- lib/google_url_signatures.rb
+++ lib/google_url_signatures.rb
@@ -1,6 +1,23 @@
+# Based on the code at:
+# http://gmaps-samples.googlecode.com/svn/trunk/urlsigning/urlsigner.rb
+
+require 'base64'
+require 'uri'
+# hmac, and hmac-sha1, are from # gem install ruby-hmac
+require 'hmac'
+require 'hmac-sha1'
+
 module GoogleUrlSignatures
   
-  def self.sign(url, key)
-    
+  def self.sign(key, url)
+    uri = URI.parse url
+
+    path_and_query = uri.path + '?' + uri.query
+
+    decoded_key = Base64.decode64(key.tr('-_','+/'))
+    decoded_signature = (HMAC::SHA1.new(decoded_key) << path_and_query).digest
+    signature = Base64.encode64(decoded_signature).tr('+/','-_')
+
+    "#{uri.scheme}://#{uri.host}#{path_and_query}&signature=#{signature}"
   end
 end
--- test/test_google_url_signatures.rb
+++ test/test_google_url_signatures.rb
@@ -1,7 +1,19 @@
 require 'helper'
 
 class TestGoogleUrlSignatures < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+
+  context "signing" do
+    context "with a valid key and url" do
+      setup {
+        @url = "http://maps.google.com/maps/api/geocode/json?address=New+York&sensor=false&client=ABC123"
+        @key = "vNIXE0xscrmjlyV-12Nj_BvUPaw="
+        @signed = GoogleUrlSignatures.sign(@key, @url)
+      }
+      should "generate signed url" do
+        assert_equal "http://maps.google.com/maps/api/geocode/json?address=New+York&sensor=false&client=ABC123&signature=Wjm6Tt9aygtehoibWXjkD1DqEH4=\n",
+                     @signed
+      end
+    end
   end
+
 end
